157f9b41819fbb0b6a3ccd4052214f428a7a5ea5
Narendra <narendra21399@gmail.com>
2019-02-11 18:57:32 +0530

    angular task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeDeatailsComponent } from './employee-deatails/employee-deatails.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
+const appRoutes = [
+  { path: '', component: EmployeeDeatailsComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeDeatailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
